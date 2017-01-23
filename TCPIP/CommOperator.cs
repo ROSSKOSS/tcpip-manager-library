affdc5e6116965dcc7cf73a116ee6010dc1dd3fb
Rost Kostenko <hugebro@gmail.com>
2017-01-23 22:34:55 +0200

    initial commit

--- TCPIP/CommOperator.cs
+++ TCPIP/CommOperator.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Net;
 namespace TCPIP
 {
     /// <summary>
-    /// 
+    /// The Server-part class. Manages the incoming requests and distributes them to the listeners.
     /// </summary>
-    public class TCPIPManager
+    public class CommOperator
     {
-
-
+        
     }
 }
